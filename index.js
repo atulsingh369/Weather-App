a1527696390656b9b4c22443df76cfc75901fd0f
atulsingh369 <businessatul7684@gmail.com>
2023-03-02 03:02:35 +0530

    updated

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const temp = document.getElementById("temp");
+const loc = document.getElementById("loc");
+const time = document.getElementById("time");
+const emoji = document.getElementById("emoji");
+const emjType = document.getElementById("emjType");
+const input = document.querySelector("input");
+
+let target = "Lucknow";
+
+const disp = (data) => {
+	loc.textContent = data.location.name;
+	time.textContent = data.location.localtime;
+	temp.textContent = data.current.temp_c;
+	const sup = document.createElement("sup");
+	sup.classList.add("deg")
+	sup.textContent = "o";
+	temp.appendChild(sup);
+	emjType.textContent = data.current.condition.text;
+	emoji.src = data.current.condition.icon;
+}
+
+const fetchData = async () => {
+	const url = `https://api.weatherapi.com/v1/current.json?key=0196414b3b9144f9a26200354230103&q=${target}`;
+	const response = await fetch(url);
+	const data = await response.json()
+		.then((data) => disp(data))
+		.catch((e) => {
+			alert("Enter proper name");
+			window.location.reload();
+		});
+}
+
+const find = () => {
+	target = input.value;
+	fetchData();
+	input.value = "";
+}
+window.addEventListener('load', fetchData);
